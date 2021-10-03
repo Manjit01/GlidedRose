24e42ec38b65ed39ed9ebe3ff1c88c4d731f9763
roop <roop.manjit.kaur@gmail.com>
2021-10-03 23:53:08 +1300

    basic requirements

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -7,11 +7,20 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class GildedRoseTest {
 
     @Test
-    void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
+    void testFrameworkWorks() {
+        Item[] items = new Item[]{new Item("foo", 0, 0)};
         GildedRose app = new GildedRose(items);
         app.updateQuality();
         assertEquals("foo", app.items[0].name);
     }
 
+    @Test
+    void systemLowerValues() {
+        Item[] items = new Item[]{new Item("foo", 15, 25)};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(14, app.items[0].sellIn);
+        assertEquals(24, app.items[0].quality);
+
+    }
 }
