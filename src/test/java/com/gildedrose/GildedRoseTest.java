0434682f141b8b80d5d61a5c6f6ec5cf90a80c1e
roop <roop.manjit.kaur@gmail.com>
2021-10-04 00:24:38 +1300

    pin down: agedBrie Increases In Quality

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -6,7 +6,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class GildedRoseTest {
 
-    private Item createAndUpdate(int sellIn, int quality) {
+    private Item createAndUpdate(String name, int sellIn, int quality) {
         Item[] items = new Item[]{new Item("foo", sellIn, quality)};
         GildedRose app = new GildedRose(items);
         app.updateQuality();
@@ -15,31 +15,31 @@ class GildedRoseTest {
 
     @Test
     void testFrameworkWorks() {
-        Item item = createAndUpdate(0, 17);
+        Item item = createAndUpdate("foo", 0, 17);
         assertEquals("foo", item.name);
     }
 
     @Test
     void systemLowerValues() {
-       Item item = createAndUpdate(15, 25);
+       Item item = createAndUpdate("foo", 15, 25);
         assertEquals(14, item.sellIn);
         assertEquals(24, item.quality);
 
     }
     @Test
     void qualityDegradesTwiceAsFast() {
-        Item item = createAndUpdate(0, 17);
+        Item item = createAndUpdate("foo", 0, 17);
         assertEquals(15, item.quality);
     }
 
     @Test
     void qualityIsNeverNegative() {
-      Item item = createAndUpdate(0, 0);
+      Item item = createAndUpdate("foo", 0, 0);
         assertEquals(0, item.quality);
     }
     @Test
     void agedBrieIncreasesInQuality(){
-        Item item = createAndUpdate(15, 25);
+        Item item = createAndUpdate("Aged Brie", 15, 25);
         assertEquals(26, item.quality);
     }
     }
